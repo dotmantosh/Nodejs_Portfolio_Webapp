b04905a840b5e6e55e87921de2f218bf1e48ab37
dotmantosh <dotuntosh@gmail.com>
2024-03-11 08:19:41 +0100

    schemas controllers routes services added

--- src/models/projects.schema.ts
+++ src/models/projects.schema.ts
@@ -1,20 +1,43 @@
 import { Schema, model, Document, Types } from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2'
 
+export enum ProjectTypeEnum {
+  personalProject = "personal_project",
+  contract = "contact",
+  employee = "employee"
+}
 interface ProjectDocument extends Document {
   name: string;
   description: string;
+  type: ProjectTypeEnum;
   userId: Types.ObjectId;
-  skills: Types.ObjectId[];
-  workExperienceId: Types.ObjectId;
+  stacks: Types.ObjectId[];
+  workExperienceId?: Types.ObjectId;
 }
 
 // Project Schema
 const projectsSchema = new Schema<ProjectDocument>({
   name: String,
   description: String,
-  userId: { type: Schema.Types.ObjectId, ref: 'User' },
-  skills: [{ type: Schema.Types.ObjectId, ref: 'Skill' }],
+  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  type: {type: String, enum: ["personal_project", "contract", "employee"]},
+  stacks: [{ type: Schema.Types.ObjectId, ref: 'Skill' }],
   workExperienceId: { type: Schema.Types.ObjectId, ref: 'WorkExperience' }
 });
 
-export const Projects = model<ProjectDocument>('Project', projectsSchema);
+projectsSchema.plugin(mongoosePaginate)
+
+// Add a custom validator to ensure workExperienceId is only present when type is 'employee'
+projectsSchema.path('workExperienceId').validate(function(this: ProjectDocument, value: any) {
+  return this.type === ProjectTypeEnum.employee ? !!value : true;
+}, 'workExperience is only allowed when type is employee');
+
+
+// projectsSchema.virtual('populatedSkills', {
+//   ref: 'Skill', // Reference to Skill model
+//   localField: 'skills', // Field in the WorkExperience schema
+//   foreignField: '_id', // Field in the Skill schema
+//   justOne: false // Set to false if the 'skills' field is an array
+// });
+
+export const ProjectSchema = model<ProjectDocument>('Project', projectsSchema);
--- src/models/workexperience.schema.ts
+++ src/models/workexperience.schema.ts
@@ -6,7 +6,7 @@ interface WorkExperienceDocument extends Document {
   startDate: Date;
   endDate: Date;
   userId: Types.ObjectId;
-  skills: Types.ObjectId[];
+  stacks: Types.ObjectId[];
 }
 
 // WorkExperience Schema
@@ -15,7 +15,25 @@ const workExperienceSchema = new Schema<WorkExperienceDocument>({
   startDate: Date,
   endDate: Date,
   userId: { type: Schema.Types.ObjectId, ref: 'User' },
-  skills: [{ type: Schema.Types.ObjectId, ref: 'Skill' }]
+  stacks: [{ type: Schema.Types.ObjectId, ref: 'Skill' }]
 });
 
-export const WorkExperience = model<WorkExperienceDocument>('WorkExperience', workExperienceSchema);
+// Define a pre-save hook to enforce the maximum limit
+const MAX_WORK_EXPERIENCE = 100; // Maximum number of skills allowed
+workExperienceSchema.pre('save', async function(next) {
+  const userId = this.userId
+  const workExperienceCount = await this.model('WorkExperience').countDocuments({userId});
+  if (workExperienceCount >= MAX_WORK_EXPERIENCE) {
+    throw new Error(`Maximum limit of ${MAX_WORK_EXPERIENCE} workExperience reached`);
+  }
+  next();
+});
+
+// workExperienceSchema.virtual('populatedSkills', {
+//   ref: 'Skill', // Reference to Skill model
+//   localField: 'skills', // Field in the WorkExperience schema
+//   foreignField: '_id', // Field in the Skill schema
+//   justOne: false // Set to false if the 'skills' field is an array
+// });
+
+export const WorkExperienceSchema = model<WorkExperienceDocument>('WorkExperience', workExperienceSchema);
