e4ebeafbb84d70efc5639bec320f63e4e4dc7761
dotmantosh <dotuntosh@gmail.com>
2024-03-12 08:14:04 +0100

    general routes added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import MessageResponse from "../interfaces/MessageResponse";
 import auth from './auth.route'
+import profile from './profile.route'
 
 const router = express.Router();
 
@@ -15,6 +16,7 @@ router.get < {},
     });
 
 router.use('/users', auth);
+router.use('/profile', profile);
 // router.use('/todos', todos)
 
 export default router;
