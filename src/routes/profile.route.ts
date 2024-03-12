91ba8ed90adc6d2000b9f2d0680e0f30b85871aa
dotmantosh <dotuntosh@gmail.com>
2024-03-12 11:15:43 +0100

    profile routes added

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -5,6 +5,11 @@ import MessageResponse from "../interfaces/MessageResponse";
 const router = express.Router();
 
 router.get < {},MessageResponse >("/fetch", ProfileController.fetchAllProfiles);
+router.get < {},MessageResponse >("/user/fetch", ProfileController.fetchAllProfiles);
+router.get < {},MessageResponse >("/fetch/:id", ProfileController.fetchAllProfiles);
+router.post < {},MessageResponse >("/create", ProfileController.createProfile);
+router.put < {},MessageResponse >("/update", ProfileController.updateProfile);
+router.delete < {},MessageResponse >("/delete", ProfileController.deleteProfile);
 
 
 export default router;
