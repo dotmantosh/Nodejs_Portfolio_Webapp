d497246bd99941688628fcbdf33321247cf26b96
dotmantosh <dotuntosh@gmail.com>
2024-06-15 01:37:32 +0100

    refactor to support production mongodb uri

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,10 @@
-import mongoose, { ConnectOptions } from "mongoose";
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-  console.log(process.env.MONGODB_URI);
-  
+  // console.log(process.env.MONGODB_URI_DEV);
 
-  const conn = await mongoose.connect(process.env.MONGODB_URI || '');
+
+  const conn = await mongoose.connect(process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI_PROD as string : process.env.MONGODB_URI_DEV as string);
 
   console.log(`Mongodb Connected: ${conn.connection.host}`);
 };
